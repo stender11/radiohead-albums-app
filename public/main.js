22a52de5d66ec0dfe18680e301a6bfd76355901f
Adam Stephens <astephens1134@gmail.com>
2022-06-13 11:32:35 -0400

    change fetch link

--- public/main.js
+++ public/main.js
@@ -45,7 +45,7 @@ async function deleteEntry() {
 async function apiRequest() {
     const albumName = document.querySelector("select").value
     try {
-        const response = await fetch(`http://localhost:8001/api/${albumName}`)
+        const response = await fetch(`https://radiohead-albums-app.herokuapp.com/api/${albumName}`)
         const data = await response.json()
         console.log(data)
 
